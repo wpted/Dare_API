d403763d52506c06341d5b90c2fa489a03e000c7
wpted <silverhandsome@gmail.com>
2022-06-17 23:16:17 +0800

    Create a util package for scraping dares and the randomization algorithm

--- core/util/get_dares.py
+++ core/util/get_dares.py
@@ -1,1 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+dare_website = "https://www.mantelligence.com/funny-dares/"
+
+response = requests.get(url=dare_website)
+response.raise_for_status()
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+question_list = [str(question.get_text())[3:].strip() for question in soup.find_all(class_="quest")]
+
+
+
+
+if __name__ == '__main__':
+    print(question_list)
 
